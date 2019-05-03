6f8e16850a5ecae2f6896efb1d471b44c7ab67f0
glugg23 <m.leonhardt424@gmail.com>
2019-05-03 15:53:36 +0100

    Add frequencies to priority queue

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,29 @@
 use std::collections::btree_map::BTreeMap;
+use std::collections::binary_heap::BinaryHeap;
+
+#[derive(Debug, Eq, PartialEq)]
+enum NodeKind {
+    Internal(Box<Node>, Box<Node>),
+    Leaf(char)
+}
+
+#[derive(Debug, Eq, PartialEq)]
+struct Node {
+    freq: usize,
+    kind: NodeKind
+}
+
+impl Ord for Node {
+    fn cmp(&self, rhs: &Self) -> std::cmp::Ordering {
+        rhs.freq.cmp(&self.freq)
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, rhs: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(&rhs))
+    }
+}
 
 fn main() {
     let text = "test";
@@ -7,5 +32,14 @@ fn main() {
 
     for ch in text.chars() {
         *freq.entry(ch).or_insert(0) += 1;
+    }
+
+    let mut prior_freq = BinaryHeap::new();
+
+    for entry in freq {
+        prior_freq.push(Node {
+            freq: entry.1,
+            kind: NodeKind::Leaf(entry.0)
+        });
     }
 }
